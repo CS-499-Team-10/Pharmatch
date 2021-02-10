004820e0d451ee68fecfe3126d0c9c799ec1e126
Jed <jamijares3@gmail.com>
2021-02-09 21:53:29 -0600

    adds prefab and generates it

---
+++ Assets/TileCreator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileCreator : MonoBehaviour
+{   
+    public GameObject Tile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Tile, new Vector3(0, 0, 0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
