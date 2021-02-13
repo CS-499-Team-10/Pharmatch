0b5e80c25e1957b67da978011c856eef3ec93c3c
Jed <jamijares3@gmail.com>
2021-02-13 15:11:47 -0600

    creates multiple cards

---
+++ Assets/Scripts/DrugTile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrugTile : MonoBehaviour {
+	// [SerializeField] private GameObject cardBack;
+	[SerializeField] private SceneController controller;
+
+	private int _id;
+	public int id {
+		get {return _id;}
+	}
+
+	public void SetCard(int id, Sprite image) {
+		_id = id;
+		GetComponent<SpriteRenderer>().sprite = image;
+	}
+
+	public void OnMouseDown() {
+		// if (cardBack.activeSelf && controller.canReveal) {
+		// // if (cardBack.activeSelf){
+		// 	cardBack.SetActive(false);
+		// 	// controller.CardRevealed(this);
+		// }
+	}
+
+	public void Unreveal() {
+		// cardBack.SetActive(true);
+	}
+}
