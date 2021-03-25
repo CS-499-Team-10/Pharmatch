3cd9d1784b5d3482a2a297aed50045867298b16b
Jed <jamijares3@gmail.com>
2021-03-25 15:03:03 -0500

    fixes tile disappearing bug

--- Assets/Scripts/DrugTile.cs
+++ Assets/Scripts/DrugTile.cs
@@ -9,6 +9,8 @@ public class DrugTile : MonoBehaviour {
 	[SerializeField] public List<string> drugMatches;
 	[SerializeField] public SceneController controller;
 
+	public bool markedToDestroy = false;
+
 	private const float moveSpeed = 2500;
 	private const float scaleSpeed = 10;
 
@@ -26,8 +28,16 @@ public class DrugTile : MonoBehaviour {
 			transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, moveSpeed * Time.deltaTime);
 		}
 		// tiles are spawned at scale 0 and grow to 1
-		if(transform.localScale != Vector3.one) {
+		if(!markedToDestroy && (transform.localScale != Vector3.one)) {
 			transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.one, scaleSpeed * Time.deltaTime);
+		}
+
+		if(markedToDestroy && (transform.localScale != Vector3.zero)) {
+			transform.localScale = Vector3.MoveTowards(transform.localScale, Vector3.zero, scaleSpeed * Time.deltaTime);
+		}
+
+		if(markedToDestroy && (transform.localScale == Vector3.zero)) {
+			GameObject.Destroy(this.gameObject);
 		}
 	}
 
@@ -53,18 +63,12 @@ public class DrugTile : MonoBehaviour {
 		GetComponent<Image>().color = new Color(186F/255F, 195F/255F, 121F/255F);
 	}
 
-    IEnumerator wait(RectTransform newCell)
+    IEnumerator Wait(RectTransform newCell)
     {
         controller.audios.Play();
         yield return new WaitForEndOfFrame();
-        // transform.parent = newCell;
-        while (transform.localScale.x > .1f && transform.localScale.y > .1f)
-        {
-            transform.localScale -= new Vector3(0.2f, 0.2f, 0.2f);
-            yield return new WaitForEndOfFrame();
-        }
         this.controller.tilesOnScreen.Remove(this); //remove this card from the list of active cards
-        GameObject.Destroy(this.gameObject); //and destroy it
+		markedToDestroy = true;
         yield return 0;
     }
 
@@ -97,9 +101,9 @@ public class DrugTile : MonoBehaviour {
 				transform.parent = newCell;
 				foreach (Transform child in newCell) {
 					controller.tilesOnScreen.Remove(child.GetComponent<DrugTile>()); //remove this tile from the list of active tiles
-					GameObject.Destroy(child.gameObject); //and destroy it
+					child.GetComponent<DrugTile>().markedToDestroy = true;
  				}
-                StartCoroutine(wait(newCell));
+                StartCoroutine(Wait(newCell));
 				return true;
 			}
 		}
