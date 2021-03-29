edfbf4ce34a12254a96fbd8c4998ae0a0e30b8d7
Jed <jamijares3@gmail.com>
2021-03-28 19:05:12 -0500

    spawn time fluctuates with number of tiles on screen

--- Assets/Scripts/TimedTapController.cs
+++ Assets/Scripts/TimedTapController.cs
@@ -7,20 +7,43 @@ public class TimedTapController : SceneController
     protected DrugTile firstSelected;
     protected DrugTile secondSelected;
 
+    float spawnCooldown = 2f; // seconds until the next tile will spawn
+    float timeSinceSpawn = 0f; // seconds since last tile spawned
+
+    [SerializeField] const float minSpawnTime = 0.75f;
+    [SerializeField] const float maxSpawnTime = 4f;
+
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        for (int i = 0; i < GetCells().Length / 2; i++)
+        // for (int i = 0; i < GetCells().Length / 2; i++)
+        // {
+        //     CreateCard();
+        // }
+    }
+
+    // maps a value s from a1-a2 to b1-b2
+    float MapRange(float s, float a1, float a2, float b1, float b2)
+    {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
+    }
+
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime; // update timer
+        if (timeSinceSpawn >= spawnCooldown) // create card if elapsed
         {
             CreateCard();
         }
-        InvokeRepeating("CreateCard", 1, 1);
+
+        spawnCooldown = MapRange(tilesOnScreen.Count, 0, GetCells().Length, minSpawnTime, maxSpawnTime);
     }
 
     protected void CreateCard()
     {
         if (tilesOnScreen.Count == GetCells().Length) GameOver();
+        timeSinceSpawn = 0;
         base.CreateCard();
     }
 
