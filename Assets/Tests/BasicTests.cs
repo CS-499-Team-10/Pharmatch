d0e0a8606acf5a42d7e5588b2f5dbf4e3afb525c
Jed <jamijares3@gmail.com>
2021-03-31 15:20:32 -0500

    adds failing tests

--- Assets/Tests/BasicTests.cs
+++ Assets/Tests/BasicTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
 
 namespace Tests
 {
@@ -13,16 +14,35 @@ namespace Tests
         public void BasicTestsSimplePasses()
         {
             // Use the Assert class to test conditions
+            SceneManager.LoadScene("MatchGame");
+            GameObject sceneController = GameObject.Find("SceneController");
+            if (sceneController == null)
+            {
+                Assert.Fail();
+            }
         }
 
-        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-        // `yield return null;` to skip a frame.
-        [UnityTest]
-        public IEnumerator BasicTestsWithEnumeratorPasses()
+        // A Test behaves as an ordinary method
+        [Test]
+        public void BasicTestsSimpleFails()
         {
-            // Use the Assert class to test conditions.
-            // Use yield to skip a frame.
-            yield return null;
+            // Use the Assert class to test conditions
+            SceneManager.LoadScene("MatchGame");
+            GameObject sceneController = GameObject.Find("UI");
+            if (sceneController == null)
+            {
+                Assert.Fail();
+            }
         }
+
+        // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // // `yield return null;` to skip a frame.
+        // [UnityTest]
+        // public IEnumerator BasicTestsWithEnumeratorPasses()
+        // {
+        //     // Use the Assert class to test conditions.
+        //     // Use yield to skip a frame.
+        //     yield return null;
+        // }
     }
 }
