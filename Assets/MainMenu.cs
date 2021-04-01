f053f58f869e3c968baae9a2257a48f38d310585
Jed <jamijares3@gmail.com>
2021-03-31 21:21:29 -0500

    adds mode selector method

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
+        ModeSelector.mode = ModeSelector.Mode.Timed; // set a certain game mode
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
---
+++ Assets/Scripts/ModeSelector.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModeSelector : MonoBehaviour
+{
+    public enum Mode
+    {
+        Tap,
+        Timed,
+        Slide
+    }
+
+    [SerializeField] public static Mode mode = Mode.Tap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SelectMode();
+    }
+
+    public void SelectMode(Mode newMode)
+    {
+        mode = newMode;
+        SelectMode();
+    }
+
+    public void SelectMode()
+    {
+        switch (mode)
+        {
+            case Mode.Tap:
+                gameObject.GetComponent<TapController>().enabled = true;
+                gameObject.GetComponent<TimedTapController>().enabled = false;
+                gameObject.GetComponent<SlideController>().enabled = false;
+                break;
+            case Mode.Timed:
+                gameObject.GetComponent<TapController>().enabled = false;
+                gameObject.GetComponent<TimedTapController>().enabled = true;
+                gameObject.GetComponent<SlideController>().enabled = false;
+                break;
+            case Mode.Slide:
+                gameObject.GetComponent<TapController>().enabled = false;
+                gameObject.GetComponent<TimedTapController>().enabled = false;
+                gameObject.GetComponent<SlideController>().enabled = true;
+                break;
+        }
+    }
+}
