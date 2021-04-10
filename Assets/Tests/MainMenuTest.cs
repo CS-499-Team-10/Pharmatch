1a7b41b8bb614cd5e2acdbd789468a4707257690
Jed <jamijares3@gmail.com>
2021-04-10 12:01:00 -0500

    adds tests

---
+++ Assets/Tests/MainMenuTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class MainMenuTest
+    {
+        [UnityTest]
+        public IEnumerator LoadTimed()
+        {
+            SceneManager.LoadScene("GameMenu");
+            while (SceneManager.GetActiveScene().name != "GameMenu")
+                yield return null; // wait for the scene to load
+
+            // get and press play button
+            GameObject playButtonObject = GameObject.Find("PlayTimedButton");
+            var playButton = playButtonObject.GetComponent<Button>();
+            playButton.onClick.Invoke();
+
+            // did correct scene load?
+            while (SceneManager.GetActiveScene().name != "MatchGame")
+                yield return null; // wait for the scene to load
+            Assert.IsTrue(SceneManager.GetActiveScene().name == "MatchGame");
+
+            // check game mode
+            GameObject sceneController = GameObject.Find("SceneController");
+            SceneController controllerComponent = sceneController.GetComponent<TimedTapController>();
+            Assert.IsTrue(controllerComponent.enabled);
+            controllerComponent = sceneController.GetComponent<TapController>();
+            Assert.IsFalse(controllerComponent.enabled);
+            controllerComponent = sceneController.GetComponent<SlideController>();
+            Assert.IsFalse(controllerComponent.enabled);
+
+        }
+    }
+}
