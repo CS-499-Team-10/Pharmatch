053914edbac1dc6d0d1be6d99b18d4c85319f921
Jed <jamijares3@gmail.com>
2021-04-10 12:14:21 -0500

    condense basic tests

--- Assets/Tests/BasicTests.cs
+++ Assets/Tests/BasicTests.cs
@@ -12,37 +12,21 @@ namespace Tests
         [UnityTest]
         public IEnumerator FindSceneController()
         {
-            const string sceneName = "MatchGame";
-            SceneManager.LoadScene(sceneName);
-            // wait for the scene to load before proceeding
-            while (SceneManager.GetActiveScene().name != sceneName)
-            {
-                yield return null;
-            }
-            GameObject sceneController = GameObject.Find("SceneController");
-            if (sceneController == null)
-            {
-                Assert.Fail();
-            }
-            // yield return null;
+            SceneManager.LoadScene("MatchGame");
+            while (SceneManager.GetActiveScene().name != "MatchGame")
+                yield return null; // wait for the scene to load
+
+            Assert.NotNull(GameObject.Find("SceneController"));
         }
 
         [UnityTest]
         public IEnumerator FailToFindGameObject()
         {
-            const string sceneName = "MatchGame";
-            SceneManager.LoadScene(sceneName);
-            // wait for the scene to load before proceeding
-            while (SceneManager.GetActiveScene().name != sceneName)
-            {
-                yield return null;
-            }
-            GameObject obj = GameObject.Find("NotHere");
-            if (obj != null)
-            {
-                Assert.Fail();
-            }
-            // yield return null;
+            SceneManager.LoadScene("MatchGame");
+            while (SceneManager.GetActiveScene().name != "MatchGame")
+                yield return null; // wait for the scene to load
+
+            Assert.Null(GameObject.Find("NotHere"));
         }
     }
 }
