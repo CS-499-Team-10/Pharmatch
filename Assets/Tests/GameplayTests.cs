7f2b8d348e62580cfaa827af146f51edfb00d946
Jed <jamijares3@gmail.com>
2021-04-11 10:55:42 -0500

    test edits

--- Assets/Tests/GameplayTests.cs
+++ Assets/Tests/GameplayTests.cs
@@ -14,12 +14,13 @@ namespace Tests
         {
             ModeSelector.mode = ModeSelector.Mode.Timed;
             SceneManager.LoadScene("MatchGame");
+            yield return new WaitForSeconds(1);
             while (SceneManager.GetActiveScene().name != "MatchGame")
                 yield return null; // wait for the scene to load
 
             GameObject sceneController = GameObject.Find("SceneController");
             SceneController controllerComponent = sceneController.GetComponent<TimedTapController>();
-            Time.timeScale = 100f; // speed up and wait for game to end
+            Time.timeScale = 99.0f; // speed up and wait for game to end
             while (SceneManager.GetActiveScene().name != "GameMenu")
                 yield return null; // wait for the scene to load
             Assert.True(SceneManager.GetActiveScene().name == "GameMenu");
@@ -30,6 +31,7 @@ namespace Tests
         {
             ModeSelector.mode = ModeSelector.Mode.Tap;
             SceneManager.LoadScene("MatchGame");
+            yield return new WaitForSeconds(1);
             while (SceneManager.GetActiveScene().name != "MatchGame")
                 yield return null; // wait for the scene to load
 
@@ -39,15 +41,15 @@ namespace Tests
             // check that each tile has a match available
             foreach (var tile in controllerComponent.tilesOnScreen)
             {
-                bool foundMatch = false;
+                int foundMatches = 0;
                 foreach (var otherTile in controllerComponent.tilesOnScreen)
                 {
                     if (tile.CheckMatch(otherTile))
                     {
-                        foundMatch = true;
+                        foundMatches += 1;
                     }
                 }
-                Assert.True(foundMatch);
+                Assert.True(foundMatches == 1);
             }
         }
     }
