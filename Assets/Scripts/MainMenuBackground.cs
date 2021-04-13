17ff9f5bd43d811e9690bb183307f6f7b2dddd15
Jed <jamijares3@gmail.com>
2021-04-13 15:06:37 -0500

    adds simple animations when loading the game

---
+++ Assets/Scripts/MainMenuBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuBackground : MonoBehaviour
+{
+    float alpha = 1.0f;
+    [SerializeField] Image background;
+
+    // Update is called once per frame
+    void Update()
+    {
+        alpha -= 1f * (float)Time.deltaTime;
+        if (alpha <= 0f) Destroy(this.gameObject);
+        background.color = new Color(1.0f, 1.0f, 1.0f, alpha);
+    }
+}
---
+++ Assets/Scripts/ReturnToCenter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToCenter : MonoBehaviour
+{
+    void Awake()
+    {
+        StartCoroutine(MoveTowards(transform, new Vector3(transform.localPosition.x, 0f, 0f), 0.25f));
+    }
+
+    // wrapper for MoveTowards to use as a coroutine
+    // edited from https://stackoverflow.com/a/51166030
+    IEnumerator MoveTowards(Transform objectToMove, Vector3 toPosition, float duration)
+    {
+        float counter = 0;
+
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            Vector3 currentPos = objectToMove.localPosition;
+
+            float time = Vector3.Distance(currentPos, toPosition) / (duration - counter) * Time.deltaTime;
+
+            objectToMove.localPosition = Vector3.MoveTowards(currentPos, toPosition, time);
+
+            // Debug.Log(counter + " / " + duration);
+            yield return null;
+        }
+        objectToMove.localPosition = toPosition;
+    }
+}
