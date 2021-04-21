428206c1da0e06bc7cb458e9204b72b11fa77544
Jed <jamijares3@gmail.com>
2021-04-21 10:58:53 -0500

    fixes hint mode toggle

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -1,32 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
-    
+    [SerializeField] GameObject toggleHints;
+    void Start()
+    {
+        UpdateToggleSwitch();
+    }
 
-    public void ChangeHintModeValue() {
+    public void ChangeHintModeValue()
+    {
         //testing
         string output = "Hint Mode is set to: ";
         string change = "Hint Mode Changed to: ";
-        if (PlayerPrefs.GetInt("HintMode") == 0) {
+        if (toggleHints.GetComponent<Toggle>().isOn)
+        {
             output += PlayerPrefs.GetInt("HintMode");
             Debug.Log(output);
             PlayerPrefs.SetInt("HintMode", 1);
             change += PlayerPrefs.GetInt("HintMode");
             Debug.Log(change);
-
         }
-        else {
+        else
+        {
             output += PlayerPrefs.GetInt("HintMode");
             Debug.Log(output);
             PlayerPrefs.SetInt("HintMode", 0);
             change += PlayerPrefs.GetInt("HintMode");
             Debug.Log(change);
         }
+        // UpdateToggleSwitch();
+    }
 
-        
-
+    void UpdateToggleSwitch()
+    {
+        bool value = (PlayerPrefs.GetInt("HintMode") == 1) ? true : false;
+        if (value != toggleHints.GetComponent<Toggle>().isOn)
+            toggleHints.GetComponent<Toggle>().isOn = value;
     }
 }
