8d00958fa5f9de1ca3625ae17efbde139904ec49
jbbr245 <70719171+jbbr245@users.noreply.github.com>
2021-04-21 20:22:53 -0400

    Added Music
    Added music for each game mode, as well as music for the home screen (home screen music is the same as for match mode, it was Dr. Peabody's favorite of the 3)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,20 +5,50 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] public AudioClip tap;
+    [SerializeField] public AudioClip timed;
+    [SerializeField] public AudioClip slide;
+
+    private GameObject music;
+
+    public void Awake()
+    {
+        music = GameObject.Find("BGM");
+        if(music.GetComponent<AudioSource>().clip != tap)
+        {
+            music.GetComponent<AudioSource>().clip = tap;
+            music.GetComponent<AudioSource>().Play();
+        }
+    }
 
     public void PlayTapGame()
     {
         ModeSelector.mode = ModeSelector.Mode.Tap; // set a certain game mode
+        if (music.GetComponent<AudioSource>().clip != tap)
+        {
+            music.GetComponent<AudioSource>().clip = tap;
+            music.GetComponent<AudioSource>().Play();
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void PlayTimedGame()
     {
         ModeSelector.mode = ModeSelector.Mode.Timed; // set a certain game mode
+        if (music.GetComponent<AudioSource>().clip != timed)
+        {
+            music.GetComponent<AudioSource>().clip = timed;
+            music.GetComponent<AudioSource>().Play();
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
     public void PlaySlideGame()
     {
         ModeSelector.mode = ModeSelector.Mode.Slide; // set a certain game mode
+        if (music.GetComponent<AudioSource>().clip != slide)
+        {
+            music.GetComponent<AudioSource>().clip = slide;
+            music.GetComponent<AudioSource>().Play();
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
